d97923fef3a1bfcd025d4388acde714b265e9082
SunSet97 <sun974029@ajou.ac.kr>
2021-06-15 15:17:17 +0900

    애니메이션 BlendTree 프로토타입 세팅
    -기본 Locomotion/Idle/up Stairs/Seat 애니메이션 세팅.
    -인스펙터 창에서 캐릭터 action, situation변수 적용할 수 있도록 스크립트 적용.
    -Invoke 수정(10초마다 situation과 action 변수 변경)

--- Assets/Animation/Main_Animation.cs
+++ Assets/Animation/Main_Animation.cs
@@ -9,18 +9,35 @@ using System;
 public class Main_Animation : MonoBehaviour
 {
     public Animator anim;
+    public Transform char_Transform;
+    [Header("Charcter Animation Properties")]
     [SerializeField] private float speed;
+    [SerializeField] private float direction;
+    [SerializeField] private bool seat;
+    [Space(10f)]
+    [Tooltip("Action//0:Idle 1:Locomotion 2:Seat 3:Jump 4:Walking Up Stairs")]
+    [SerializeField] private int action_count;
+    [SerializeField] private int situation_count;
+    [Space(10f)]
+    [Header("Current State")]
     [SerializeField] private int action;
     [SerializeField] private int situation;
-    [SerializeField] private bool seat;
+
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
-        speed = anim.speed;
-        InvokeRepeating("Action", 1.0f,260f);
+        speed = anim.GetFloat("Speed");
+        direction= anim.GetFloat("Direction");
+        action = anim.GetInteger("action");
+        situation = anim.GetInteger("situation");
+        if (action_count>0)//action의 수가 4개 이상일 때.
+            InvokeRepeating("Action", 1.0f,10f);
+        else//action의 수가 3개이하 있을 때
+            InvokeRepeating("Situation", 1.0f, 10f);
     }
 
     // Update is called once per frame
@@ -28,22 +45,30 @@ public class Main_Animation : MonoBehaviour
     {
         if (speed != 0)//움직이고 있을 때 //캐릭터의 속도 anim.speed
         {
-            anim.SetBool("seat", false);
-            anim.SetFloat("speed", speed);
-            
+            anim.SetBool("Seat", false);
+            anim.SetFloat("Speed", speed);
+            anim.SetFloat("Direction", direction);
+
         }
-        
+        if (speed == 0) 
+        {
+            anim.SetInteger("action", action);
+            anim.SetInteger("situation", situation);
+        }
+            
+
     }
     void Action()
     {
-        action = UnityEngine.Random.Range(0, 4);
+        action = UnityEngine.Random.Range(0, action_count);
+        
         anim.SetInteger("action", action);
         Debug.Log("Action" + action);
-        InvokeRepeating("Situation",1.0f,20f);
+        InvokeRepeating("Situation",1.0f,10f);
     }
     void Situation()
     {
-        situation = UnityEngine.Random.Range(0, 4);
+        situation = UnityEngine.Random.Range(0, situation_count);
         anim.SetInteger("situation", situation);
         Debug.Log("Situation" + situation);
 
@@ -55,14 +80,20 @@ public class Main_Animation : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Chair"))
         {
+            anim.bodyPosition = collision.gameObject.transform.position;//캐릭터 위치를 의자에 세팅.
+
             anim.SetBool("Seat", true);
+        }
+        if (collision.gameObject.CompareTag("Stairs"))
+        {
+            anim.SetInteger("action", 1);
         }
     }
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Chair"))
+        if (collision.gameObject.CompareTag("Stairs"))
         {
-            anim.SetBool("Seat", false);
+            anim.SetInteger("action", 0);
         }
     }
 }
