67c225beceb18a3a21c8bfbe1c36b9cfa392000c
SunSet97 <sun974029@ajou.ac.kr>
2021-08-07 18:53:08 +0900

    NaviAgent이동 설정, 애니메이션 스크립트 수정
    -NavAgent속도와 애니메이터 BlendTree통일
    -Navimesh 베이크
    -랜덤으로 캐릭터가 이동할 Target세팅(환경 세팅, 스크립트 설정)
    -애니메이션 설정 횟수가 일정 조건에 도달할 때, action비활성화 한 뒤 다른 타겟을 찾도록 만듦.

--- Assets/Animation/Main_Animation.cs
+++ Assets/Animation/Main_Animation.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-
+using UnityEngine.AI;
 
 [Serializable]
 
 public class Main_Animation : MonoBehaviour
 {
     public Animator anim;
+    Animation animation;
     public Transform char_Transform;
+    NavMeshAgent agent;
     [Header("Charcter Animation Properties")]
     [SerializeField] private float speed;
     [SerializeField] private float direction;
+    [SerializeField] private int target_num;
+    [SerializeField] private GameObject[] Targets;
     [SerializeField] private bool seat;
+    private bool action;
     [Space(10f)]
+    [Header("Current State")]
+    [SerializeField] private int action_num;
+    [SerializeField] private int situation_num;
+    [Space(10f)]
+    bool isPicking;
+    [SerializeField] GameObject playerEquipPoint;
+    /*
     [Tooltip("Action//0:Idle 1:Locomotion 2:Seat 3:Jump 4:Walking Up Stairs")]
     [SerializeField] private int action_count;
     [SerializeField] private int situation_count;
-    [Space(10f)]
-    [Header("Current State")]
-    [SerializeField] private int action;
-    [SerializeField] private int situation;
-    bool isPicking;
-    [SerializeField] GameObject playerEquipPoint;
-
+    [Space(10f)]*/
 
     void Awake() 
     {
@@ -34,39 +40,91 @@ public class Main_Animation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponentInChildren<Animator>();
-        speed = anim.GetFloat("Speed");
-        direction= anim.GetFloat("Direction");
-        action = anim.GetInteger("action");
-        situation = anim.GetInteger("situation");
-        /*if (action_count>0)//action의 수가 4개 이상일 때.
-            InvokeRepeating("Action", 1.0f,10f);
-        else//action의 수가 3개이하 있을 때
-            InvokeRepeating("Situation", 1.0f, 10f);*/
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        animation = GetComponent<Animation>();
+        speed = agent.speed;
+        direction = agent.angularSpeed*Mathf.Deg2Rad;//각도>라디안
+        action_num = anim.GetInteger("action");
+        situation_num = anim.GetInteger("situation");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (speed != 0)//움직이고 있을 때 //캐릭터의 속도 anim.speed
+        if (speed != 0&& action)//움직이고 있을 때 //캐릭터의 속도 anim.speed
         {
+            anim.SetInteger("action",0);//기본상태
             anim.SetBool("Seat", false);
             anim.SetFloat("Speed", speed);
             anim.SetFloat("Direction", direction);
+            findTarget();//타겟으로 이동하도록 만듦
 
         }
-        if (speed == 0) 
+        if (speed == 0&& action) 
         {
-            anim.SetInteger("action", action);
-            anim.SetInteger("situation", situation);
+            anim.SetInteger("situation", situation_num);
         }
+
         if (isPicking) 
         {
             Drop();
         }
-
-
     }
+    //Navigation세팅
+    #region
+    public void findTarget()//타겟 위치 찾아서 방향을 잡는 것
+    {
+        target_num= UnityEngine.Random.Range(0,Targets.Length);
+        agent.SetDestination(Targets[target_num].transform.position);
+    }
+    public void OnTriggerEnter(Collider other)//타겟에 부딪히면 세팅된 애니메이션 실행
+    {
+        if (other.gameObject.CompareTag("Target")) 
+        {
+            action = true;
+            speed = 0;
+            char_Transform.LookAt(other.gameObject.transform);//target방향으로 바라보게 만들기
+            SetAnim(other.gameObject.name);//object의 애니메이션 number를 함수에 보냄
+        }
+    }
+    public void SetAnim(string action_number)//부딪힌 오브젝트에 세팅된 애니메이션 재생
+    {
+        action_num= int.Parse(action_number);//string>int
+        anim.SetInteger("action", action_num);//애니메이션 재생
+        if (anim.GetCurrentAnimatorStateInfo(0).loop)//현재 재생중인 애니메이션이 Loop일 때. 
+        {
+            InvokeRepeating("Situation", 1.0f, 5f);
+        }
+        else //한번 재생되는 애니메이션이면 한번 재생된 이후 action 비활성화
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) 
+            {
+                action = false;
+            }
+        }
+        ㄴ
+    }
+    void Situation()//애니메이터 SubStateMachine에 있는 종속 애니메이션 실행
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 5.0f)//재생된 횟수가 5번 이상되버리면 action끄기 
+        {
+            action = false;
+        }
+        else
+        {
+            if (action)
+            {
+                situation_num = UnityEngine.Random.Range(0, 4);
+                anim.SetInteger("situation", situation_num);
+
+            }
+            //Debug.Log("Situation" + situation);
+        }
+    }
+    #endregion
+    //물건집기
+    #region
     public void Pickup(GameObject item)
     {
         SetEquip(item, true);
@@ -93,48 +151,5 @@ public class Main_Animation : MonoBehaviour
 
         itemRigidbody.isKinematic = isEquip;
     }
-
-    #region
-    /*
-   
-    void Action()
-    {
-        action = UnityEngine.Random.Range(0, action_count);
-        
-        anim.SetInteger("action", action);
-        Debug.Log("Action" + action);
-        InvokeRepeating("Situation",1.0f,10f);
-    }
-    void Situation()
-    {
-        situation = UnityEngine.Random.Range(0, situation_count);
-        anim.SetInteger("situation", situation);
-        Debug.Log("Situation" + situation);
-
-    }
-
-   
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Chair"))
-        {
-            anim.bodyPosition = collision.gameObject.transform.position;//캐릭터 위치를 의자에 세팅.
-
-            anim.SetBool("Seat", true);
-        }
-        if (collision.gameObject.CompareTag("Stairs"))
-        {
-            anim.SetInteger("action", 1);
-        }
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Stairs"))
-        {
-            anim.SetInteger("action", 0);
-        }
-    }*/
-    #endregion
-
+    #endregion//물건집기
 }
