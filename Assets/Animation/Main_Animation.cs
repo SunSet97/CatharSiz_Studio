369e763a5b3700f93e951ea1a726cb94ecdf2a49
SunSet97 <sun974029@ajou.ac.kr>
2021-08-13 16:37:00 +0900

    AI 수정
    -애니메이션 Loop인지, 애니메이션이 끝났는지 인지
    -AI이동 방향 설정 방법 수정
    -애니메이션 작동시 navAgent비활성화

--- Assets/Animation/Main_Animation.cs
+++ Assets/Animation/Main_Animation.cs
@@ -55,15 +55,18 @@ public class Main_Animation : MonoBehaviour
         //anim.SetFloat("Direction", Mathf.Cos(agent.gameObject.transform.rotation.y));
         if (action==false)//애니메이션이 끝났을 때
         {
+            anim.SetBool("Action", false);
             anim.SetInteger("action",0);//기본상태
+            findTarget();//타겟으로 이동하도록 만듦
             anim.SetFloat("Speed", speed);
             Degree();
             anim.SetFloat("Direction", direction);
-            findTarget();//타겟으로 이동하도록 만듦
         }
         else
         {
-            anim.SetInteger("situation", situation_num);
+            set_animation();
+            //anim.SetBool("Action", true);
+            //anim.SetInteger("situation", situation_num);
         }
 
         if (isPicking) 
@@ -77,18 +80,39 @@ public class Main_Animation : MonoBehaviour
         Vector3 Char_direction = new Vector3(char_Transform.rotation.x, 0, char_Transform.rotation.z);
         Vector3 minus = Agent_direection - Char_direction;
         float Rad = Mathf.Atan2(minus.y, minus.x);
-        Debug.Log(Rad);
         direction = Rad;
     }
     //Navigation세팅
     #region
     public void findTarget()//타겟 위치 찾아서 방향을 잡는 것
     {
-        if (target_num == 0 || (action==false && speed==0))//타겟넘버 세팅 안되어있을 때 혹은 멈춰있는 상태에 false일 때
+        if (target_num == 0)//타겟넘버 세팅 안되어있을 때 혹은 멈춰있는 상태에 false일 때
         {
-            target_num = UnityEngine.Random.Range(1, Targets.Length+1);//1~5까지 타겟 세팅
+            if (action == false)
+            {
+                int temp = target_num;
+                target_num = UnityEngine.Random.Range(1, Targets.Length + 1);//1~5까지 타겟 세팅
+                Debug.Log(target_num);
+                if (target_num == temp)
+                {
+                    Debug.Log("뭐야0");
+                    while (target_num == temp)
+                    {
+                        target_num = UnityEngine.Random.Range(1, Targets.Length + 1);//1~5까지 타겟 세팅
+                        temp = target_num;
+                    }
+                    agent.SetDestination(Targets[target_num - 1].transform.position);
+                }
+            }
         }
-        agent.SetDestination(Targets[target_num-1].transform.position);
+        else
+        {
+            if (action == false)
+            {
+                agent.SetDestination(Targets[target_num - 1].transform.position);
+
+            }
+        }
     }
 
     public void OnTriggerEnter(Collider other)
@@ -98,11 +122,11 @@ public class Main_Animation : MonoBehaviour
         {
             Debug.Log(other.name);
             action = true;
+            anim.SetBool("Action",action);
             agent.transform.rotation= other.gameObject.transform.localRotation;
             agent.transform.position = other.gameObject.transform.position;
-            agent.isStopped = true;
-            agent.velocity = Vector3.zero;
-            agent.ResetPath();
+            //agent.velocity = Vector3.zero;
+            agent.enabled=false;
             SetAnim(other.gameObject.name);//object의 애니메이션 number를 함수에 보냄
         }
     }
@@ -113,34 +137,48 @@ public class Main_Animation : MonoBehaviour
     {
         action_num= int.Parse(action_number);//
         anim.SetInteger("action", action_num);//애니메이션 재생
-        if (anim.GetCurrentAnimatorStateInfo(0).loop)//현재 재생중인 애니메이션이 Loop일 때. 
+        if (anim.GetCurrentAnimatorStateInfo(0).loop && anim.GetCurrentAnimatorStateInfo(0).IsName("0"))//현재 재생중인 애니메이션이 Loop일 때. 
         {
+            Debug.Log("뭔데1");
             InvokeRepeating("Situation", 1.0f, 5f);
         }
         else //한번 재생되는 애니메이션이면 한번 재생된 이후 action 비활성화
         {
-            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f) 
-            {
-                action = false;
-                agent.isStopped = false;
-                 //AgentSetting();
-            }
+            Debug.Log("뭔데2");
+            anim.SetInteger("situation", 0);
         }
-        
+    }
+    public void set_animation() 
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("1")&& anim.GetCurrentAnimatorStateInfo(0).normalizedTime> 0.8f)
+         {
+            
+            action = false;
+            anim.SetBool("Action", action);
+            agent.enabled = true;
+            Debug.Log(action);
+            agent.ResetPath();
+        }
+
     }
     void Situation()//애니메이터 SubStateMachine에 있는 종속 애니메이션 실행
     {
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 2.0f)//재생된 횟수가 2번 이상되버리면 action끄기 
         {
             action = false;
+            anim.SetBool("Action", action);
+            anim.SetInteger("action", 0);
+            agent.enabled = true;
+            agent.ResetPath();
         }
         else
         {
             if (action)
             {
                 situation_num = UnityEngine.Random.Range(0, 4);
+                agent.enabled = true;
                 anim.SetInteger("situation", situation_num);
-
+                agent.ResetPath();
             }
             //Debug.Log("Situation" + situation);
         }
